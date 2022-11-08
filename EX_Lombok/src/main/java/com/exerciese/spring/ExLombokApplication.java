4195f5bc09fade4a5e2449278405500d185ea978
superssol <hansole10@gmail.com>
2022-11-08 20:32:28 +0900

    first commit

---
+++ EX_Lombok/src/main/java/com/exerciese/spring/ExLombokApplication.java
@@ -0,0 +1,13 @@
+package com.exerciese.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExLombokApplication.class, args);
+	}
+
+}
---
+++ EX_Lombok/src/main/java/com/exerciese/spring/Member.java
@@ -0,0 +1,10 @@
+package com.exerciese.spring;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+	private String id;
+	private String name;
+}
